30505bbb0adfa6f2c393dcfbca2ebdf0354b553d
super999 <sup9@qq.com>
2025-12-26 17:36:01 +0800

    重构项目结构，更新主窗口和启动脚本，替换 MuseLog 为 DiabloClicker

--- DiabloClicker/main.py
+++ DiabloClicker/main.py
@@ -11,22 +11,23 @@ import sys
 from PySide6.QtWidgets import QApplication
 from qt_material import list_themes, apply_stylesheet
 
-from MuseLog import logging_utils
-from MuseLog.main_window import MuseLogMainWindow
-from MuseLog.services.global_services import init_services
+from DiabloClicker import logging_utils
+from DiabloClicker.main_window import DiabloClickerMainWindow
+
+
 
 def main():
     # 初始化日志
     logging_utils.init_logging()
     logging.info("Meta Story Board started")
     # 初始化全局服务
-    init_services()
+    # init_services()
     # 启动主窗口
     app = QApplication(sys.argv)
     print(list_themes())
     # 套用“dark_teal.xml”深色主题
     apply_stylesheet(app, theme='dark_cyan.xml')
-    window = MuseLogMainWindow()
+    window = DiabloClickerMainWindow()
     window.show()
     sys.exit(app.exec())
 
--- DiabloClicker/ui/main_window_ui.py
+++ DiabloClicker/ui/main_window_ui.py
@@ -15,54 +15,93 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QToolButton, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QHBoxLayout, QListView,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 590)
+        MainWindow.resize(1083, 692)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.centralwidget.setStyleSheet(u"\n"
-"    QWidget#centralwidget {\n"
-"        background-color: rgba(208, 208, 255, 1);\n"
-"        border: 1px solid black;\n"
-"    }\n"
-"    ")
+        self.centralwidget.setStyleSheet(u"")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setSpacing(2)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(1, 1, 1, 1)
         self.widget_L01 = QWidget(self.centralwidget)
         self.widget_L01.setObjectName(u"widget_L01")
-        self.widget_L01.setMaximumSize(QSize(16777215, 100))
-        self.widget_L01.setStyleSheet(u"\n"
-"       QWidget#widget_L01 {\n"
-"           background-color: rgb(200, 200, 200);\n"
-"       }\n"
-"       QToolButton#toolButton_screenshot {\n"
-"           background-color: rgb(180, 0, 0);\n"
-"           border: 1px solid black;\n"
-"           border-radius: 20px;\n"
-"       }\n"
-"       ")
-        self.toolButton_screenshot = QToolButton(self.widget_L01)
-        self.toolButton_screenshot.setObjectName(u"toolButton_screenshot")
-        self.toolButton_screenshot.setGeometry(QRect(30, 20, 71, 61))
+        self.widget_L01.setMaximumSize(QSize(16777215, 60))
+        self.widget_L01.setStyleSheet(u"")
+        self.pushButton_screenshot = QPushButton(self.widget_L01)
+        self.pushButton_screenshot.setObjectName(u"pushButton_screenshot")
+        self.pushButton_screenshot.setGeometry(QRect(20, 0, 71, 61))
+        self.pushButton_test = QPushButton(self.widget_L01)
+        self.pushButton_test.setObjectName(u"pushButton_test")
+        self.pushButton_test.setGeometry(QRect(110, 0, 71, 61))
 
         self.verticalLayout.addWidget(self.widget_L01)
 
         self.widget_L02 = QWidget(self.centralwidget)
         self.widget_L02.setObjectName(u"widget_L02")
+        self.widget_L02.setStyleSheet(u"\n"
+"        QWidget#widget_L02 {\n"
+"            border: 1px solid gray;\n"
+"        }\n"
+"       ")
+        self.verticalLayout_2 = QVBoxLayout(self.widget_L02)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.widget = QWidget(self.widget_L02)
+        self.widget.setObjectName(u"widget")
+        self.widget.setMinimumSize(QSize(0, 50))
+        self.widget.setMaximumSize(QSize(16777215, 50))
+        self.checkBoxStartMonitor = QCheckBox(self.widget)
+        self.checkBoxStartMonitor.setObjectName(u"checkBoxStartMonitor")
+        self.checkBoxStartMonitor.setGeometry(QRect(30, 15, 131, 20))
+
+        self.verticalLayout_2.addWidget(self.widget)
+
+        self.widget_2 = QWidget(self.widget_L02)
+        self.widget_2.setObjectName(u"widget_2")
+        self.horizontalLayout = QHBoxLayout(self.widget_2)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.widget_3 = QWidget(self.widget_2)
+        self.widget_3.setObjectName(u"widget_3")
+
+        self.horizontalLayout.addWidget(self.widget_3)
+
+        self.widget_4 = QWidget(self.widget_2)
+        self.widget_4.setObjectName(u"widget_4")
+        self.horizontalLayout_2 = QHBoxLayout(self.widget_4)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.listView = QListView(self.widget_4)
+        self.listView.setObjectName(u"listView")
+
+        self.horizontalLayout_2.addWidget(self.listView)
+
+
+        self.horizontalLayout.addWidget(self.widget_4)
+
+        self.horizontalLayout.setStretch(0, 1)
+        self.horizontalLayout.setStretch(1, 1)
+
+        self.verticalLayout_2.addWidget(self.widget_2)
+
 
         self.verticalLayout.addWidget(self.widget_L02)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        self.menubar.setGeometry(QRect(0, 0, 1083, 33))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -75,6 +114,8 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.toolButton_screenshot.setText(QCoreApplication.translate("MainWindow", u"\u622a\u56fe", None))
+        self.pushButton_screenshot.setText(QCoreApplication.translate("MainWindow", u"\u622a\u56fe", None))
+        self.pushButton_test.setText(QCoreApplication.translate("MainWindow", u"\u6d4b\u8bd5", None))
+        self.checkBoxStartMonitor.setText(QCoreApplication.translate("MainWindow", u"\u542f\u52a8\u76d1\u63a7", None))
     # retranslateUi
 
